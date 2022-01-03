ca9fc702ac33f0eda172d8c5603c0e298c3166f7
KARAN SANJAY KAJROLKAR <karankajrolkar@gmail.com>
2022-01-03 15:43:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def predict_api():
     else:
         return render_template('index.html')
 
-if __name__=="__main__":
-    print("------**app started**---------")
-    app.run(debug=True)
+#if __name__=="__main__":
+#    print("------**app started**---------")
+#    app.run(debug=True)
 
 
